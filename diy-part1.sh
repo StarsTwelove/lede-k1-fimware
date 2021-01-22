8d409ac0b1db9c3cb0e3b7be19554863e8e214e7
junpermini <58542166+junpermini@users.noreply.github.com>
2021-01-21 23:02:16 -0600

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/coolsnowwolf/packages' feeds.conf.default
+sed -i '$a src-git lienol src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
