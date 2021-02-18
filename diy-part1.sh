6f821316e5b1b489f3101e58eec893d35e82cb52
junpermini <58542166+junpermini@users.noreply.github.com>
2021-02-18 10:25:25 -0600

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,7 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/coolsnowwolf/packages' feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
