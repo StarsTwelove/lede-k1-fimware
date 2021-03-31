2baa8aa841ae2523390131b874f1b7a0f76b53ac
princekunmain <58542166+princekunmain@users.noreply.github.com>
2021-03-31 05:48:34 -0500

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,26 +10,5 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# 
-device_name='PSG1208'
-wifi_name='PHICOMM'
-lan_ip='192.168.2.1'        # Lan Ip地址
-utc_name='Asia\/Shanghai'   # 时区
-default_theme='argon'
-
- 
-# 修改机器名称
-echo "设置主机名"
-sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
-
-# 设置lan ip
-echo "设置lan ip"
-sed -i "s/192.168.2.1/$lan_ip/g" package/base-files/files/bin/config_generate
-
-# 修改默认时区
-echo "修改时区"
-sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
-     
-# 修改wifi名称
-echo "修改wifi名称"
-sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
